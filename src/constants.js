f334d0505d246421bde073d13f4c899f2672c2d0
Karthiik26 <imkarthiik26@gmail.com>
2025-04-20 19:31:02 +0530

    commit final

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,5 @@
+// export const BACKEND_URI = "http://localhost:5000/api";
+
 export const BACKEND_URI = "https://online-lecture-schedule-management.onrender.com/api";
 
 // it is not a right way to use this we bcz it is not secure
